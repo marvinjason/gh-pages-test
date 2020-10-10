0abd8daf0b62498910d1597d78f6492269d8fc6e
marvinjason <marvinjason.sy@gmail.com>
2020-10-11 00:30:57 +0800

    Updates

---
+++ precache-manifest.1ad7ef3a906c30584f5bf745fd8ae30e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c00f99e58dcd4dc7900817e605fdcb",
+    "url": "/gh-pages-test/index.html"
+  },
+  {
+    "revision": "5e2a8943e8c5df868ab4",
+    "url": "/gh-pages-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9454dea3541eb99d2642",
+    "url": "/gh-pages-test/static/js/2.4c89f74e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages-test/static/js/2.4c89f74e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2a8943e8c5df868ab4",
+    "url": "/gh-pages-test/static/js/main.a57472f1.chunk.js"
+  },
+  {
+    "revision": "00706ac92bcf9de8c299",
+    "url": "/gh-pages-test/static/js/runtime-main.656322dd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
